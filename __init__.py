a20fd29d26a1e9c81ef18c3d560135572d182c59
Bjorn Hogberg <bjorn.hogberg@gmail.com>
2020-11-03 19:24:25 +0100

    Setting up pt. 1

---
+++ __init__.py
@@ -0,0 +1,17 @@
+bl_info = {
+    "name": "caDNAno2Blender",
+    "author": "Björn Högberg",
+    "version": (1, 0, 0),
+    "blender": (2, 83, 0),
+    "location": "View3D",
+    "description": "Module for working with caDNAno files in Blender",
+    "category": "Import-Export",
+}
+
+
+def register():
+    pass
+
+
+def unregister():
+    pass
